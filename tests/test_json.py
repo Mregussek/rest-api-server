3785fea8a8396fa9e648ed930095cd2390a78947
Mateusz Rzeczyca <mateuszrzeczyca17@gmail.com>
2020-03-12 20:18:08 +0100

    jsonhandler passes

--- tests/test_json.py
+++ tests/test_json.py
@@ -2,7 +2,6 @@ import unittest
 import sys
 sys.path.append("..")
 
-import json
 from JSONHandler import JSONHandler
 
 
@@ -34,6 +33,13 @@ class JsonTestCase(unittest.TestCase):
             ]
         }
 
+        self.new_book = {
+            "id": "04",
+            "language": "New book",
+            "edition": "asd",
+            "author": "Mateusz Rzeczyca"
+        }
+
         self.file_name = 'test_books.json'
 
     def test_reading_json(self):
@@ -42,21 +48,15 @@ class JsonTestCase(unittest.TestCase):
         self.assertEqual(json_handler.get_books_list(), self.json_data['books'])
 
     def test_appending_new_book_json(self):
-        new_book = {
-                        "id": "04",
-                        "language": "New book",
-                        "edition": "asd",
-                        "author": "Mateusz Rzeczyca"
-                    }
-
         json_handler = JSONHandler(self.json_data)
-        self.assertTrue(json_handler.append_new_element(new_book))
+        self.assertTrue(json_handler.append_new_element(self.new_book))
 
-        self.json_data['books'].append(new_book)
+        self.json_data['books'].append(self.new_book)
         self.assertEqual(self.json_data['books'], json_handler.get_books_list())
 
     def test_reading_new_book(self):
         json_handler = JSONHandler(self.json_data)
+        json_handler.append_new_element(self.new_book)
         self.assertTrue(json_handler.write_json(self.file_name))
         self.assertEqual(self.json_data, json_handler.read_json(self.file_name))
 
